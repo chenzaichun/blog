a20d02eff2e65da7bcc002bde75503c50daff298
Chen, Zai-Chun <chenzaichun@gmail.com>
2015-08-17 15:31:24 +0800

    modify for 404

--- app.js
+++ app.js
@@ -25,58 +25,53 @@
 // console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
 // app.listen(port, hostname);
 
-var sys = require("sys");
-var http = require("http");
-var url = require("url");
-var path = require("path");
-var fs = require("fs");
-var port = process.env.PORT || 3000;
+// var sys = require("sys");
+// var http = require("http");
+// var url = require("url");
+// var path = require("path");
+// var fs = require("fs");
+// var port = process.env.PORT || 3000;
 
 
-var app = http.createServer(function(req,res){
-  var uri = url.parse(req.url).pathname;
-  var filename = path.join(process.cwd()+'/public',uri);
-  fs.exists(filename, function(exists){
-    if(!exists){
-      filename = path.join(process.cwd()+'/public/404.html');
-    }
+// var app = http.createServer(function(req,res){
+//   var uri = url.parse(req.url).pathname;
+//   var filename = path.join(process.cwd()+'/public',uri);
+//   fs.exists(filename, function(exists){
+//     if(!exists){
+//       filename = path.join(process.cwd()+'/public/404.html');
+//     }
 
-    fs.readFile(filename, "binary",function(err,file){
-    if(err){
-      res.writeHead(500,{"Content-Type":"text/plain"});
-      res.end(err+"\n");
-      return;
-    }
+//     fs.readFile(filename, "binary",function(err,file){
+//     if(err){
+//       res.writeHead(500,{"Content-Type":"text/plain"});
+//       res.end(err+"\n");
+//       return;
+//     }
 
-    res.writeHead(200);
-    res.end(file,"binary");
-    });
-  });
-}).listen(port);
+//     res.writeHead(200);
+//     res.end(file,"binary");
+//     });
+//   });
+// }).listen(port);
 
-console.log("Server running on port %d", port);
+// console.log("Server running on port %d", port);
 
-// var connect = require('connect'),
-//   app = connect(),
-//   port = process.env.PORT;
+var connect = require('connect');
+var app = connect(),
+var port = process.env.PORT;
+var compression = require('compression');
+var serveStatic = require('serve-static')
+app.use(compression());
+app.use(serveStatic(__dirname + '/public'));
+app.use(function(req, res, next){
+  switch (req.url) {
+  default:
+    console.log("redirect 404");
+    res.redirect('/404.html');
+    break;
+  }
+});
 
-// var compression = require('compression');
-// var serveStatic = require('serve-static')
-// app.use(compression());
-// app.use(serveStatic(__dirname + '/public'));
-// app.use(function(req, res, next){
-//   switch (req.url) {
-//   case '/404':
-//     console.log("redirect 404");
-//     res.redirect('/404.html');
-//     break;
-//   default:
-//     var body = 'my page';
-//     res.setHeader('Content-Length', body.length);
-//     res.end(body);
-//   }
-// });
-
-// app.listen(port, function(){
-//   console.log('Hexo is running on port %d', port);
-// });
+app.listen(port, function(){
+  console.log('Hexo is running on port %d', port);
+});
